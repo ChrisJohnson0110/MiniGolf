45b2b0015e81dd7d93220d657a6ee6afdc55d869
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-04-07 17:08:35 +0100

    Models

---
+++ Assets/Scripts/MenuSettings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSettings : MonoBehaviour
+{
+    [SerializeField] private Settings GameSettings = new Settings();
+
+    //UI elements reference
+
+    //Audio
+    [SerializeField] private Slider volumeSlider;
+
+    //Graphics
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void VolumeSlider()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings
+{
+    float volume;
+    GraphicsQuality graphics;
+
+
+
+
+
+    enum GraphicsQuality 
+    { 
+        Low,
+        Medium,
+        High
+    }
+
+}
