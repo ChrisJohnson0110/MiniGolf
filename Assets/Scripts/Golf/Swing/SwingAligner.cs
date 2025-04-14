778c90717cf80308c0ace62c940a6f8b86d8e989
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-04-14 23:40:17 +0100

    Update SwingAligner.cs

--- Assets/Scripts/Golf/Swing/SwingAligner.cs
+++ Assets/Scripts/Golf/Swing/SwingAligner.cs
@@ -12,7 +12,7 @@ public class SwingAligner : MonoBehaviour
         // Keep aligner at ball's position
         Vector3 pos = ballTransform.position;
         //pos.y = transform.position.y; 
-        pos.y = ballTransform.position.y + 0.5f;
+        pos.y = ballTransform.position.y + 1.5f;
         transform.position = pos;
 
         // Handle input
